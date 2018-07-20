70d2395851f093cc96d0e3f30a8dd8d86142f290
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-07-20 18:03:58 +0300

    local ci fixes

--- shell/local-ci.sh
+++ shell/local-ci.sh
@@ -8,8 +8,8 @@ eval $(docker-machine env default --shell bash)
 docker build -t $IMAGE --build-arg DOTNET_CONFIG=Build ../app
 kubectl config set-context minikube
 docker push $IMAGE
+    # -s env.PORTAL_URL=http://localhost:7000 \
 # ./helm-deploy.sh -i postgres -r iddb -c ../kube/iddb
 ./helm-deploy.sh -i $IMAGE -r $PROJ -c ../kube/$PROJ \
     -s env.ASPNETCORE_ENVIRONMENT=Development \
-    -s env.PORTAL_URL=http://192.168.1.44:30740 \
-    -s env.SCHEMES_URL=http://192.168.1.44:7001 \
+    -s env.PORTAL_URL=http://192.168.1.35:31067 \
