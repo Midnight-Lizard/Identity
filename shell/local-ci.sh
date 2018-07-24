201dfed194936e8abd8ac125251a68132c1d3761
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-07-24 16:00:39 +0300

    switching to debezium/postgres

--- shell/local-ci.sh
+++ shell/local-ci.sh
@@ -9,7 +9,7 @@ docker build -t $IMAGE --build-arg DOTNET_CONFIG=Build ../app
 kubectl config set-context minikube
 docker push $IMAGE
     # -s env.PORTAL_URL=http://localhost:7000 \
-# ./helm-deploy.sh -i postgres -r iddb -c ../kube/iddb
+./helm-deploy.sh -i debezium/postgres:10.0 -r iddb -c ../kube/iddb
 ./helm-deploy.sh -i $IMAGE -r $PROJ -c ../kube/$PROJ \
     -s env.ASPNETCORE_ENVIRONMENT=Development \
     -s env.PORTAL_URL=http://192.168.1.35:31067 \
