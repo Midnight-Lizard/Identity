09809551c5e7b730cbac77c69a21ef652138d7e5
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-07-28 14:27:19 +0300

    providing default values for secrets

--- shell/local-ci.sh
+++ shell/local-ci.sh
@@ -8,9 +8,19 @@ eval $(docker-machine env default --shell bash)
 docker build -t $IMAGE --build-arg DOTNET_CONFIG=Build ../app
 kubectl config set-context minikube
 docker push $IMAGE
-./helm-deploy.sh -i debezium/postgres:10.0 -r iddb -c ../kube/iddb
+# ./helm-deploy.sh -i debezium/postgres:10.0 -r iddb -c ../kube/iddb
+secret=$(echo -n not-a-secret | base64 -w 0);
+IDENTITY_SERVER_SIGNIN_CERTIFICATE=$secret;
+IDENTITY_SERVER_SIGNIN_CERTIFICATE_PASSWORD=$secret;
+IDENTITY_GOOGLE_CLIENT_ID=$secret;
+IDENTITY_GOOGLE_CLIENT_SECRET=$secret;
+IDENTITY_PORTAL_CLIENT_SECRET=$secret;
 ./helm-deploy.sh -i $IMAGE -r $PROJ -c ../kube/$PROJ \
     --set env.ASPNETCORE_ENVIRONMENT=Development \
-    --set secrets.portal.clientSecret=not-a-secret \
+    --set secrets.signinCertificate.data=$IDENTITY_SERVER_SIGNIN_CERTIFICATE \
+    --set secrets.signinCertificate.password=$IDENTITY_SERVER_SIGNIN_CERTIFICATE_PASSWORD \
+    --set secrets.google.clientId=$IDENTITY_GOOGLE_CLIENT_ID \
+    --set secrets.google.clientSecret=$IDENTITY_GOOGLE_CLIENT_SECRET \
+    --set secrets.portal.clientSecret=$IDENTITY_PORTAL_CLIENT_SECRET \
     --set env.PORTAL_URL=http://192.168.1.35:31067 \
     # --set env.PORTAL_URL=http://localhost:7000 \
