07a7a0a375da3099597f12e6727250bb749f4189
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-07-31 11:29:21 +0300

    using cert pass from file

--- app/Configuration/Certificate.cs
+++ app/Configuration/Certificate.cs
@@ -13,18 +13,16 @@ namespace MidnightLizard.Web.Identity.Configuration
     public static class Certificate
     {
         private static readonly string _certificatePath = "/etc/secret/certificate";
+        private static readonly string _certificatePassPath = "/etc/secret/password";
         public static X509Certificate2 Get(IConfiguration configuration, ILogger logger)
         {
-            var certPass = configuration.GetValue<string>("IDENTITY_SERVER_SIGNIN_CERTIFICATE_PASSWORD");
             if (!File.Exists(_certificatePath))
             {
                 return null;
             }
             try
             {
-                logger.LogError("certPath=[{secret}]", certPass);
-                logger.LogError("cert=[{secret}]", File.ReadAllText(_certificatePath));
-                return new X509Certificate2(_certificatePath, certPass,
+                return new X509Certificate2(_certificatePath, File.ReadAllText(_certificatePassPath),
                     X509KeyStorageFlags.Exportable | X509KeyStorageFlags.MachineKeySet);
             }
             catch (Exception ex)
