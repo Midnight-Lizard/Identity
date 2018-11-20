9882f53f086f1aaaf113591bbe939c687c06e186
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-11-20 17:40:37 +0300

    switching to new disk

--- shell/create-gce-disk.ps1
+++ shell/create-gce-disk.ps1
@@ -1,6 +1,6 @@
 gcloud compute `
     --project=midnight-lizard-2018 `
-    disks create ml-small-disk `
+    disks create ml-small-disk-2 `
     --zone=us-central1-a `
     --type=pd-standard `
     --size=10GB
