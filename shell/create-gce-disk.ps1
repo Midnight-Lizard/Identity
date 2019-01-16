d494b53e9ffc7111b120d1afbdc2bedd8de3f27b
Pavel Agarkov <pavel.agarkov@gmail.com>
2019-01-16 16:37:02 +0300

    first deployment

--- shell/create-gce-disk.ps1
+++ shell/create-gce-disk.ps1
@@ -1,6 +1,5 @@
-gcloud compute `
-    --project=midnight-lizard-2018 `
-    disks create ml-small-disk-2 `
+gcloud compute disks create ml-small-disk-2 `
+    --project=midnight-lizard-2019 `
     --zone=us-central1-a `
     --type=pd-standard `
     --size=10GB
